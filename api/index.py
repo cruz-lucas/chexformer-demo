268b78ed309d104bbe7587756f44531b33cbc5e6
Lucas Cruz <lucasdearaujocruz@gmail.com>
2024-01-08 02:00:00 -0300

    refactor: remove nextjs to simple html serving with fastapi

--- api/index.py
+++ api/index.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/", StaticFiles(directory=".", html=True), name="static")
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello asd"}
+
 
 @app.get("/api/python")
-def hello_world():
+async def main():
     return {"message": "Hello World"}
